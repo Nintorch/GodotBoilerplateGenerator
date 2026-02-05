b18bb815c8c83e78cb26a02db028bced69eea98b
Nintorch <92302738+Nintorch@users.noreply.github.com>
2026-02-05 20:03:57 +0500

    Fix Vector and radian handling

--- main.py
+++ main.py
@@ -185,6 +185,7 @@ def cmd_bind_methods(line):
         "float": "FLOAT",
         "int": "INT",
         "PackedByteArray": "PACKED_BYTE_ARRAY",
+        "PackedInt32Array": "PACKED_INT32_ARRAY",
         "Vector3": "VECTOR3"
     }
     for prop in class_properties:
@@ -260,17 +261,15 @@ def class_end(line):
         if len(prop) == 2:
             code_output += f"\t{prop[1]} = p_{prop[1]};\n"
         elif len(prop) == 3 and prop[2] == "rad":
-            code_output += f"\t{prop[1]} = Math::fmod(p_{prop[1]}, (float)Math::PI*2);\n"
+            code_output += f"\t{prop[1]} = Math::fmod(p_{prop[1]}, (float)Math::TAU);\n"
             code_output += f"\tif ({prop[1]} < 0.0f) " + "{\n"
-            code_output += f"\t\t{prop[1]} += (float)Math::PI*2;\n"
+            code_output += f"\t\t{prop[1]} += (float)Math::TAU;\n"
             code_output += "\t}\n"
         elif len(prop) == 4:
             if prop[0] == "float" or prop[0] == "int":
                 code_output += f"\t{prop[1]} = CLAMP(p_{prop[1]}, {prop[2]}, {prop[3]});\n"
             elif prop[0] == "Vector3":
-                code_output += f"\t{prop[1]}.x = CLAMP(p_{prop[1]}.x, {prop[2]}, {prop[3]});\n"
-                code_output += f"\t{prop[1]}.y = CLAMP(p_{prop[1]}.y, {prop[2]}, {prop[3]});\n"
-                code_output += f"\t{prop[1]}.z = CLAMP(p_{prop[1]}.z, {prop[2]}, {prop[3]});\n"
+                code_output += f"\t{prop[1]} = p_{prop[1]}.clampf({prop[2]}, {prop[3]});\n"
             else:
                 code_output += f"\n\n(Unknown type for clamp: {prop[0]})\n\n"
         else:
